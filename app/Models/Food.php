48b59a27800d871a4ccbb03ade7515c58f3b8cb5
massndp <bayu8988.88@gmail.com>
2021-09-06 14:20:35 +0700

    Softdalates in Food Model

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -5,11 +5,12 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Storage;
 
 class Food extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'name', 'description', 'ingredients', 'price', 'rate', 'type', 'picturePath'
